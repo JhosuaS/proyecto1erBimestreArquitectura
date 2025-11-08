aff99c62f0946948f759a81cc132d43edbdace87
edisonogonaga <151202618+edisonogonaga@users.noreply.github.com>
2025-11-08 17:17:38 -0500

    Update Complemento.java

--- src/operaciones/Complemento.java
+++ src/operaciones/Complemento.java
@@ -36,14 +36,14 @@ public class Complemento {
             char bit = numero.charAt(i);
 
             if (encontrado) {
-                // Después del primer 1: invertir los bits
+  
                 if (bit == '0') {
                     c2 = '1' + c2;
                 } else {
                     c2 = '0' + c2;
                 }
             } else {
-                // Copiamos bits hasta encontrar el primer 1
+                
                 c2 = bit + c2;
                 if (bit == '1') {
                     encontrado = true; 
@@ -53,5 +53,64 @@ public class Complemento {
 
         return c2;
     }
+    //Paso B (Verificación)
+  public static int ca2ADecimal(String numero) {
+        if (!Conversion.esValido(numero, 0, 2)) {
+            System.out.println("Error: el número no es binario.");
+            return 0;
+        }
+
+        boolean esNegativo = numero.charAt(0) == '1'; 
+
+        if (!esNegativo) {
+    
+            int decimal = 0;
+            int potencia = 1;
+            for (int i = numero.length() - 1; i >= 0; i--) {
+                if (numero.charAt(i) == '1') {
+                    decimal += potencia;
+                }
+                potencia *= 2;
+            }
+            return decimal;
+        } else {
+
+            String c1 = "";
+            for (int i = 0; i < numero.length(); i++) {
+                char bit = numero.charAt(i);
+                if (bit == '0') c1 += '1';
+                else c1 += '0';
+            }
+
+            // 2. Sumar 1 manualmente
+            String c2 = "";
+            int suma = 1;
+            for (int i = c1.length() - 1; i >= 0; i--) {
+                char bit = c1.charAt(i);
+                if (bit == '0' && suma == 1) {
+                    c2 = '1' + c2;
+                    suma = 0;
+                } else if (bit == '1' && suma == 1) {
+                    c2 = '0' + c2;
+                    suma = 1;
+                } else {
+                    c2 = bit + c2;
+                }
+            }
+
+            // 3. Convertir el resultado a decimal
+            int valor = 0;
+            int potencia = 1;
+            for (int i = c2.length() - 1; i >= 0; i--) {
+                if (c2.charAt(i) == '1') {
+                    valor += potencia;
+                }
+                potencia *= 2;
+            }
+
+            return -valor;
+        }
+    }
+
     
 }
