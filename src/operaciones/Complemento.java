bc539a4166268f5bfeb5199af45a1dddd169cab9
edisonogonaga <151202618+edisonogonaga@users.noreply.github.com>
2025-11-08 16:37:05 -0500

    Refactor complemento1 method to use Conversion.esValido

--- src/operaciones/Complemento.java
+++ src/operaciones/Complemento.java
@@ -2,25 +2,10 @@ package operaciones;
 
 public class Complemento {
     private boolean signo;
-    private static final int[] numeroBits = {8, 16, 32};
-
-    //TODO reemplazar metodo esBinario por Conversion.esValido
-    private static boolean esBinario(String numero) {
-    for (int i = 0; i < numero.length(); i++) {
-        char caracter = numero.charAt(i); 
-        if (caracter != '0' && caracter != '1') {
-            System.out.println("El número contiene un carácter no binario: '" + caracter + "'");
-            return false;
-        }
-    }
-    System.out.println("El número es binario.");
-    return true;
-}
-
-           //complemento 1 
-
-         public static String complemento1(String numero) {
-        if (!esBinario(numero)) {
+    private static final int[] numeroBits = {8, 16, 32
+                                            
+ public static String complemento1(String numero) {
+        if (!Conversion.esValido(numero, 0, 2)) { 
             return "Error: el número ingresado no es binario.";
         }
 
@@ -35,5 +20,6 @@ public class Complemento {
         }
         return c1;
     }
+    
     
 }
