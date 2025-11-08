df6de3cea4a7ad65d71bba6f850f048651430d78
JhosuaS <jhosua_saa2412@hotmail.com>
2025-11-08 16:17:41 -0500

    Merged changes to main to local branch

--- src/operaciones/Complemento.java
+++ src/operaciones/Complemento.java
@@ -4,6 +4,7 @@ public class Complemento {
     private boolean signo;
     private static final int[] numeroBits = {8, 16, 32};
 
+    //TODO reemplazar metodo esBinario por Conversion.esValido
     private static boolean esBinario(String numero) {
     for (int i = 0; i < numero.length(); i++) {
         char caracter = numero.charAt(i); 
