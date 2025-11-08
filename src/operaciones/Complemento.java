d3f2e91262c46037b6b900fbf2d8fe727ac3cfc2
edisonogonaga <151202618+edisonogonaga@users.noreply.github.com>
2025-11-08 14:44:05 -0500

    Implement binary number validation method
    Added a method to validate binary numbers.

--- src/operaciones/Complemento.java
+++ src/operaciones/Complemento.java
@@ -1,5 +1,17 @@
 package operaciones;
 
 public class Complemento {
-    
+    private boolean signo;
+    private static final int[] numeroBits = {8, 16, 32};
+
+    private static boolean esBinario(String numero) {
+    for (int i = 0; i < numero.length(); i++) {
+        char caracter = numero.charAt(i); 
+        if (caracter != '0' && caracter != '1') {
+            System.out.println("El número contiene un carácter no binario: '" + caracter + "'");
+            return false;
+        }
+    }
+    System.out.println("El número es binario.");
+    return true;
 }
